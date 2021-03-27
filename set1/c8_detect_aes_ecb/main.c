8c8a75e8087c1607c9abf3f8c05dea78a052e366
Jamesbarford <jamesbarfordevans@gmail.com>
2021-03-27 17:51:04 +0000

    corrected string termination

--- set1/c8_detect_aes_ecb/main.c
+++ set1/c8_detect_aes_ecb/main.c
@@ -9,22 +9,23 @@ static int detect_ecb_mode(mmapd_file_t *mmapd_file, unsigned int key_length) {
 	unsigned int line_len = get_line_len(mmapd_file);
 	unsigned int blocks_size = line_len / key_length;
 	char cur_line[line_len];
-	char block1[key_length+1];
-	char block2[key_length+1];
+	char block1[key_length + 1];
+	char block2[key_length + 1];
 	memset(cur_line, '\0', line_len);
 
 	for (unsigned int lineno = 0; lineno < mmapd_file->size / line_len; ++lineno) {
 		memcpy(cur_line, &mmapd_file->text[(lineno * line_len) + lineno], line_len);
-		cur_line[line_len+1] = '\0';
+		cur_line[line_len] = '\0';
 
 		for (unsigned int j = 0; j < blocks_size; ++j) {
 			memcpy(block1, &cur_line[j * key_length], key_length);
-			block1[key_length+1] = '\0';
+			block1[key_length] = '\0';
 
 			for (unsigned int k = j + 1; k < blocks_size; ++k) {
 				memcpy(block2, &cur_line[k * key_length], key_length);
-				block2[key_length+1] = '\0';
+				block2[key_length] = '\0';
 
+				printf("b1: %s, b2: %s\n", block1, block2);
 				if (strcmp(block1, block2) == 0) {
 					return lineno + 1; // lines in a file are not zero based
 				}
